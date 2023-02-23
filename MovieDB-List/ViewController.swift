247bdb576508e5eed2ac1c69e2b2e9699fa2b817
davin <christophorusdavin@gmail.com>
2023-02-23 19:27:10 +0700

    Initial Commit

---
+++ MovieDB-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB-List
+//
+//  Created by Christophorus Davin on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
